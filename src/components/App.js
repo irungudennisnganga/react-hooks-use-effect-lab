782a15344633a9ca0d464493b101e8d0203f09bc
+dennisngangairungu <+>
2023-11-15 12:12:16 +0300

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Question from "./Question";
 import quiz from "../data/quiz";
 
@@ -18,6 +18,7 @@ function App() {
       setScore((score) => score + 1);
     }
   }
+  
 
   return (
     <main>
